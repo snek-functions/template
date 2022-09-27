99049d58a423adf90dc14251ed47f0b8b3e492da
schettn <52858351+schettn@users.noreply.github.com>
2022-09-27 10:30:49 +0000

    fix: add types to app handler args

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import getServerlessApp from '@snek-at/functions/dist/server/getServerlessApp.js'
 
-export async function handler(event, context) {
+export async function handler(event: Object, context: Object) {
   return await getServerlessApp({
     functions: '.'
   })(event, context)
